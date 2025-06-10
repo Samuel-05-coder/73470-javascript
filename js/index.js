ea5bbc92348fca6971de2978aeface95665db5d4
Samuel-05-coder <xamuel.2005@gmail.com>
2025-06-10 10:21:14 -0500

    primer pre-entrega

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// INICIO DEL SIMULADO
+
+// VARIABLES Y CONSTANTES
+let nombreUsuario = "nombre",
+    montoPrestamo = 50000, //DOlares
+    plazoMeses = 48; 
+
+const tasaInteredFija = 0.10, // 10%
+    montoMaximoPermitido = 100000; //Dolares
