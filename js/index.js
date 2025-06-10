7fc9385db8d328e43888f0509e1948dd177af500
Samuel-05-coder <xamuel.2005@gmail.com>
2025-06-10 16:32:24 -0500

    Correcion simulador

--- js/index.js
+++ js/index.js
@@ -1,9 +1,54 @@
-// INICIO DEL SIMULADO
+// INICIO DEL SIMULADO (Prestamo personal)
 
-// VARIABLES Y CONSTANTES
-let nombreUsuario = "nombre",
-    montoPrestamo = 50000, //DOlares
-    plazoMeses = 48; 
+let nombreUsuario = prompt ("Pon tu nombre y apellido")
 
-const tasaInteredFija = 0.10, // 10%
-    montoMaximoPermitido = 100000; //Dolares
+function mostrarNombreUsuario() {
+    if (nombreUsuario){
+        alert(`Hola ${nombreUsuario}, bienvenido al simulador de prestamos personales`);
+    } else {
+        alert ("No ingresaste tu nombre, por favor intentalo de nuevo");
+    }
+}
+
+mostrarNombreUsuario();
+
+let montoIngresado = prompt ("¿Cuánto deseas pedir prestado? (Máximo $100.000)");
+montoIngresado = montoIngresado.replace(/\D/g, "");
+let montoSolicitado = parseInt (montoIngresado);
+
+const montoMaximo = 100000; // Maximo permitido
+
+function calcularPrestamo () {
+    if (!isNaN(montoSolicitado) && montoSolicitado <= montoMaximo && montoSolicitado > 0) {
+        alert ("El monto ingresado es valido")
+    } else {
+        alert ("El monto es inválido o supera el límite de $100.000. Inténtalo de nuevo.")
+    }
+}
+
+calcularPrestamo ()
+
+let plazoMeses = prompt ("Ingrese la cantidad de meses a pagar su prestamo (Maximo 68 meses)");
+plazoMeses = plazoMeses.replace(/\D/g, "");
+let mesesSolicitados = parseInt (plazoMeses);
+
+const mesesMaximos = 68; // Meses maximos permitidos
+
+function calcularMeses () {
+    if (!isNaN(mesesSolicitados) && mesesSolicitados <= mesesMaximos && mesesSolicitados > 0) {
+        alert ("La cantidad de meses ingresada es valida")
+    } else {
+        alert ("La cantidad de meses es invalida, por favor ingrese otra cantidad dentro del maxiomo")
+    }
+}
+
+calcularMeses ()
+
+//CALCULACION DE AñS DE PLAZO
+const añosPlazo = mesesSolicitados /12,
+    tasaAnual = 0.20; // 20% Anual - TASA DE INTERES ANUAL FIJA
+    interesTotal = montoSolicitado * tasaAnual * añosPlazo,
+    totalPagar = montoSolicitado + interesTotal,
+    cuotaMensual = totalPagar / mesesSolicitados;
+
+alert (`Resumen del prestamo para ${nombreUsuario}`)
